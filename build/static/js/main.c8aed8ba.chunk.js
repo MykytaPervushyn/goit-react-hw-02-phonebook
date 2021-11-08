77da882f61665a867ee197e0d2d02182d1f5142d
MykytaPervushyn <80350205+MykytaPervushyn@users.noreply.github.com>
2021-11-08 12:58:38 +0200

    Створює базову структуру

---
+++ build/static/js/main.c8aed8ba.chunk.js
@@ -0,0 +1,32 @@
+(this['webpackJsonpgoit-react-hw-02-phonebook'] =
+  this['webpackJsonpgoit-react-hw-02-phonebook'] || []).push([
+  [0],
+  {
+    11: function (e, t, c) {
+      
+      c.r(t);
+      var n = c(1),
+        o = c.n(n),
+        r = c(3),
+        s = c.n(r),
+        i = (c(8), c.p, c(9), c(0));
+      var a = function () {
+        return Object(i.jsx)('div', {
+          className: 'App',
+          children: Object(i.jsx)('header', {
+            className: 'App-header',
+            children: Object(i.jsx)('h2', { children: 'TEST' }),
+          }),
+        });
+      };
+      s.a.render(
+        Object(i.jsx)(o.a.StrictMode, { children: Object(i.jsx)(a, {}) }),
+        document.getElementById('root'),
+      );
+    },
+    8: function (e, t, c) {},
+    9: function (e, t, c) {},
+  },
+  [[11, 1, 2]],
+]);
+//# sourceMappingURL=main.c8aed8ba.chunk.js.map
